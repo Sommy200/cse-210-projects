1137fc1b5108a84e97394b93eacc99cdc425198e
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-05-09 17:30:20 +0100

    Added new lines of code

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade in percentage?: ");
+        int grade = int.Parse(Console.ReadLine());
+        string letterGrade = "";
+        if (grade >= 90) 
+        {
+            letterGrade = "A";
+            Console.WriteLine($"Your grade is {letterGrade}");
+            Console.WriteLine("Congratulations you passed the course!");
+       
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+            Console.WriteLine($"Your grade is {letterGrade}");
+            Console.WriteLine("Congratulations you passed the course!");
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+            Console.WriteLine($"Your grade is {letterGrade}");
+            Console.WriteLine("Congratulations you passed the course!");
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+            Console.WriteLine($"Your grade is {letterGrade}");
+            Console.WriteLine("You failed the course, but you can try again!");
+        }
+        else
+        {
+            letterGrade = "F";
+            Console.WriteLine($"Your grade is {letterGrade}");
+            Console.WriteLine("You failed the course, and you will need to retake it.");
+        }
     }
 }
