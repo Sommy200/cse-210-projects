30a0f049b676e660ccdd107b2427865c43b62265
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-05-09 19:00:50 +0100

    added new lines of code

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomgenerator = new Random();
+        int magicNumber = randomgenerator.Next(1, 101);
+
+        int guessCount = 0;
+        Console.Write("What is your guess? ");
+        int userGuess = int.Parse(Console.ReadLine());
+        while (userGuess != magicNumber)
+        {
+        guessCount++;
+
+        
+             if (userGuess == magicNumber)
+           {
+                Console.WriteLine("You guessed it!");
+            }
+             else if (userGuess < magicNumber)
+           {
+                Console.WriteLine("Higher");
+
+           }
+             else
+           {
+                Console.WriteLine("Lower");
+            }
+
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+             guessCount++;
+        }
+         Console.WriteLine("You guessed it!");
+         Console.WriteLine($"It took you {guessCount} guesses.");
     }
 }
