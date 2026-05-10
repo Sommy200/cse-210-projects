fc52e28427de1a8fb9fb9e29fd7a48387f9222f6
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-05-10 14:16:46 +0100

    added new lines of code

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+
+        int userFavoriteNumber = PromptUserNumber();
+
+        int squareNumber = SquareNumber(userFavoriteNumber);
+
+        DisplayResult(userName, squareNumber);
+
+    
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+    static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+    static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+    static int SquareNumber(int number)
+        {
+            int square =number * number;
+            return square;
+        }
+
+    static void DisplayResult(string name, int squareNumber)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squareNumber}");
+        }
+
     }
 }
