6b530939eb2e2a755faf6eb495a6cefca0c3815f
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-05-10 12:13:32 +0100

    added new lines of code

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numberList = new List<int>();
+        
+        Console.WriteLine("enter a list of numbers, type 0 when finished :");
+        Console.Write("Enter a number: ");
+        int userInput = int.Parse(Console.ReadLine());
+        while (userInput != 0)
+        {
+            numberList.Add(userInput);
+            Console.Write("Enter a number: ");
+            userInput = int.Parse(Console.ReadLine());
+        }
+        int sum = 0;
+        foreach (int number in numberList)
+        {
+            sum += number;
+        }
+        
+
+        double average = (double)sum / numberList.Count;
+
+        int largest = numberList[0];
+        foreach (int number in numberList)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+       
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
