b3d7768b59ba329f74508d3254f4a4497d4d8abe
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-05-15 07:15:29 +0100

    made some corrections

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -21,7 +21,7 @@ class Program
              Console.WriteLine("3. Load");
              Console.WriteLine("4. Save"); 
              Console.WriteLine("5. Quit");
-             Console.WriteLine("5. Quit");
+             
 
              Console.Write("What would you like to do? ");
              string choice = Console.ReadLine();
