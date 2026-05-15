9c562ced171789ef8cffa6a558f1dd8ac083a2e3
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-05-15 06:54:03 +0100

    add new file

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,14 @@
+public class Entry
+{
+    public string _Date;
+    public string _PromptText;
+    public string _EntryText;
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_Date}");
+        Console.WriteLine($"Prompt: {_PromptText}");
+        Console.WriteLine($"Response: {_EntryText}");
+        Console.WriteLine();
+    }
+}
