40278a3e8082da47c2d3522174b90883dd92d4d9
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-05-23 21:29:12 +0100

    added code

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        // Create a reference and scripture
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        string scriptureText = "Trust in the Lord with all thine heart and lean not unto thine own understanding in all thy ways acknowledge him and he shall direct thy paths";
+        Scripture scripture = new Scripture(reference, scriptureText);
+
+        // Main Loop 
+        bool running = true;
+
+        while (running)
+        {
+            // clear the console and display the scripture
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine();
+            // Check if scripture is completely hidden
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.WriteLine("All words are hidden, great job memorizing!");
+                Console.WriteLine("Press Enter key to exit...");
+                Console.ReadLine();
+                running = false; // leave the loop
+                break;
+
+
+
+
+            }
+
+            // prompt user
+            Console.WriteLine("Press Enter to hide more words, or type 'quit' to exit...");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                running = false; // leave the loop
+
+            }
+            else
+            {
+                scripture.HideRandomWords(3); // hide 3 more words
+            }
+
+        
+        }
+
     }
 }
