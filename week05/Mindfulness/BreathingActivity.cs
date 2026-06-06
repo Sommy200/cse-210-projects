762c21e78daed62379cd52ef950dbb6bc0de4881
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-06-06 21:11:12 +0200

    add code

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading;
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity() 
+        : base("Breathing Activity", "This activity will help you relax through breathing in and out slowly. Clear your mind and Focus on your breathing.")
+    {
+        
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("Breathe in...");
+            ShowCountdown(4);
+            Console.WriteLine();
+
+            Console.Write("Now breath out...");
+            ShowCountdown(6);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        DisplayEndingMessage();
+    }
+}
