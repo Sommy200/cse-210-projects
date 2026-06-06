fce99a4e5bf4a8a061687609255fa593d2db9975
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-06-06 16:01:35 +0200

    add code

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,49 @@
-using System;
+using System; 
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
-    }
+        Console.WriteLine("=== Homework Assignments ===");
+
+        // testing the base assignment class
+        Console.WriteLine("\n--- Base Assignment ---");
+        Assignment simpleAssignment = new Assignment("Samuel Benneth", "Multiplication");
+        Console.WriteLine(simpleAssignment.GetSummary());
+
+        // testing the maths assignment class
+        Console.WriteLine("\n--- Maths Assignment ---");
+        MathsAssignment mathAssignment = new MathsAssignment("Sommy Roberts", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        // testing the writing assignment class
+        Console.WriteLine("\n--- Writing Assignment ---");
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Poppins", "Creative Writing", "the magical world of writting");
+        
+
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        Console.WriteLine();
+
+        // test another math assignment
+        Console.WriteLine("\n--- Another Maths Assignment ---");
+        MathsAssignment anotherMathAssignment = new MathsAssignment("John Doe", "Algebra", "5.2", "1-10");
+
+        Console.WriteLine(anotherMathAssignment.GetSummary());
+        Console.WriteLine(anotherMathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        // test another writing assignment
+        Console.WriteLine("\n--- Another Writing Assignment ---");
+        WritingAssignment anotherWritingAssignment = new WritingAssignment("Jane Smith", "Essay Writing", "the impact of technology on society");
+
+        Console.WriteLine(anotherWritingAssignment.GetSummary());
+        Console.WriteLine(anotherWritingAssignment.GetWritingInformation());
+         
+
+
+
+    }   
 }
