a0c2da1f760a1aa58ffb4dfa12365359ee205ca6
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-06-07 21:08:30 +0200

    added code

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -2,72 +2,77 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 
-public class Activity
+namespace Mindfulness
 {
-    protected string _name;
-    protected string _description;
-    protected int _duration;
-
-    public Activity(string name, string description)
+    public class Activity
     {
-        _name = name;
-        _description = description;
-    }
+        protected string _name;
+        protected string _description;
+        protected int _duration;
 
-    public void DisplayStartingMessage()
-    {
-        Console.Clear();
-        Console.WriteLine($"Welcome to the {_name}. \n");
-        Console.WriteLine($"{_description}");
-        Console.WriteLine("How Long, in seconds would you like for your session?");
-        _duration = int.Parse(Console.ReadLine());
-
-        Console.Clear();
-        Console.WriteLine("Get ready...");
-        ShowSpinner(5);
-        Console.WriteLine();
-
-    }
-
-    public void DisplayEndingMessage()
-    {
-        Console.WriteLine();
-        Console.WriteLine("Well done!!");
-        ShowSpinner(3);
-        Console.WriteLine();
-        Console.WriteLine($"You have completed another {_duration} seconds of the {_name}.");
-        ShowSpinner(5);
-
-
-
-    }
-    public void ShowSpinner(int seconds)
-    {
-        List<string> animationStrings = new List<string> {"|", "/", "-", "\\"};
-        DateTime startTime = DateTime.Now;
-        DateTime endTime = startTime.AddSeconds(seconds);
-
-        int i = 0;
-        while (DateTime.Now < endTime)
+        public Activity(string name, string description)
         {
-            Console.Write(animationStrings[i]);
-            Thread.Sleep(250);
-            Console.Write("\b \b");
+            _name = name;
+            _description = description;
+        }
 
-            i++;
-            if (i >= animationStrings.Count)
+        public void DisplayStartingMessage()
+        {
+            Console.Clear();
+            Console.WriteLine($"Welcome to the {_name}. \n");
+            Console.WriteLine($"{_description}");
+            Console.WriteLine();
+            Console.Write("How long, in seconds, would you like for your session? ");
+            
+            // Fixed potential null reference and added basic parsing safety
+            string input = Console.ReadLine() ?? "0";
+            _duration = int.Parse(input);
+
+            Console.Clear();
+            Console.WriteLine("Get ready...");
+            ShowSpinner(5);
+            Console.WriteLine();
+        }
+
+        public void DisplayEndingMessage()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Well done!!");
+            ShowSpinner(3);
+            Console.WriteLine();
+            Console.WriteLine($"You have completed another {_duration} seconds of the {_name}.");
+            ShowSpinner(5);
+        }
+
+        public void ShowSpinner(int seconds)
+        {
+            List<string> animationStrings = new List<string> { "|", "/", "-", "\\" };
+            DateTime startTime = DateTime.Now;
+            DateTime endTime = startTime.AddSeconds(seconds);
+
+            int i = 0;
+            while (DateTime.Now < endTime)
             {
-                i = 0;
+                Console.Write(animationStrings[i]);
+                Thread.Sleep(250);
+                Console.Write("\b \b");
+
+                i++;
+                if (i >= animationStrings.Count)
+                {
+                    i = 0;
+                }
             }
         }
-    }
-    public void ShowCountdown(int seconds)
-    {
-        for (int i = seconds ; i > 0; i--)
+
+        public void ShowCountdown(int seconds)
         {
-            Console.Write(i);
-            Thread.Sleep(1000);
-            Console.Write("\b \b");
+            for (int i = seconds; i > 0; i--)
+            {
+                Console.Write(i);
+                Thread.Sleep(1000);
+                Console.Write("\b \b");
+            }
         }
     }
 }
