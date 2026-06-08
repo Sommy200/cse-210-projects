1e43721c64b34cb37afdd02a977b6766175d00e4
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-06-08 20:49:33 +0200

    added code

---
+++ week06/Shapes/Rectangle.cs
@@ -0,0 +1,16 @@
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double _lenght;
+        private double _width;
+
+        public Rectangle(string color, double lenght, double width) : base(color)
+        {
+            _lenght = lenght;
+            _width = width;
+        }
+
+        public override double GetArea() => _lenght * _width;
+    }
+}
