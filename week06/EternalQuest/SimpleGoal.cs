cfb4fe8cd02fa30026383881e2cc8096b74e542e
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-06-12 15:48:30 +0200

    Added code

--- week06/EternalQuest/SimpleGoal.cs
+++ week06/EternalQuest/SimpleGoal.cs
@@ -3,32 +3,43 @@ namespace EternalQuest
     public class SimpleGoal : Goal
     {
         private bool _isComplete;
-        public SimpleGoal(string name, int points) : base(name, points)
+
+        public SimpleGoal(string name, string description, int points) 
+            : base(name, description, points)
         {
             _isComplete = false;
         }
 
-        
-      
-        public override void RecordEvent()
+        public SimpleGoal(string name, string description, int points, bool isComplete) 
+            : base(name, description, points)
         {
-            _isComplete = true;
+            _isComplete = isComplete;
         }
 
-        public override string GetStatus()
+        public override int RecordEvent()
         {
-            if (_isComplete)
+            if (!_isComplete)
             {
-                return "[X]";
+                _isComplete = true;
+                return _points;
             }
-            return"[]";
+            return 0;
         }
-        
-            
-            
-        
 
-        
-        
+        public override bool IsComplete()
+        {
+            return _isComplete;
+        }
+
+        public override string GetDetailsString()
+        {
+            string checkbox = _isComplete ? "[X]" : "[ ]";
+            return $"{checkbox} {_shortName} ({_description})";
+        }
+
+        public override string GetStringRepresentation()
+        {
+            return $"SimpleGoal:{_shortName},{_description},{_points},{_isComplete}";
+        }
     }
 }
