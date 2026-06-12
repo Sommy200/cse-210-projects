a7b46054f2d09a610e8c0e1e29e30eaa1aa5bfbd
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-06-12 15:48:01 +0200

    Added code

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -2,31 +2,30 @@ namespace EternalQuest
 {
     public abstract class Goal
     {
-        private string _name;
-        private int _points;
+        protected string _shortName;
+        protected string _description;
+        protected int _points;
 
-        public Goal(string name, int points)
+        public Goal(string name, string description, int points)
         {
-            _name = name;
-            
+            _shortName = name;
+            _description = description;
             _points = points;
         }
-        public string GetName() => _name;
 
-        public int GetPoints() => _points;
+        public string GetShortName()
+        {
+            return _shortName;
+        }
 
-        // childrens compulsory version
-        public abstract void RecordEvent();
+        public int GetPoints()
+        {
+            return _points;
+        }
 
-        // children optional own version
-        public virtual string GetStatus() => "[]";
-        
-        
-            
-        
-
-        
-        
-
+        public abstract int RecordEvent();
+        public abstract bool IsComplete();
+        public abstract string GetDetailsString();
+        public abstract string GetStringRepresentation();
     }
 }
