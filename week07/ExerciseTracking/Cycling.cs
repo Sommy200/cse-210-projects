fc8c2e8a7c426eb9bb14db03425a705bab6c280b
Sommy200 <somtochukwuunabichukwu@gmail.com>
2026-06-17 09:03:20 +0200

    added code

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,30 @@
+namespace ExerciseTracker
+{
+    public class Cycling : Activity
+    {
+        private double _speed; // in mph
+
+        public Cycling(string date, int minutes, double speed)
+            : base(date, minutes)
+        {
+            _speed = speed;
+        }
+
+        public override double GetDistance()
+        {
+            // Distance = (speed * minutes) / 60
+            return (_speed * GetMinutes()) / 60;
+        }
+
+        public override double GetSpeed()
+        {
+            return _speed;
+        }
+
+        public override double GetPace()
+        {
+            // Pace = 60 / speed
+            return 60 / _speed;
+        }
+    }
+}
